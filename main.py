67a0afd430a44c622829641f4d2e018d0e27ef24
Niharika <dirisalaniharika1999@gmail.com>
2023-05-02 09:10:28 +0530

    added background

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Background
+background = pygame.image.load('background.png')
+
+
 # Caption and Icon
 pygame.display.set_caption("Space Invader")
 icon = pygame.image.load('ufo.png')
@@ -66,20 +70,29 @@ for i in range(num_of_enemies):
 # Game Loop
 running = True
 while running:
+    # RGB = Red, Green, Blue
+    screen.fill((0, 0, 0))
+    # Background Image
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            playerX_change = -5
-        if event.key == pygame.K_RIGHT:
-            playerX_change = 5
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 5
 
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-            playerX_change = 0
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
 
-    screen.fill((0,0,0))
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+        
     pygame.display.update()
 
