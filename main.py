0c82e30b612bad92890a384be2d59b8dd6514166
Niharika <dirisalaniharika1999@gmail.com>
2023-05-02 09:23:31 +0530

    added score func

--- main.py
+++ main.py
@@ -58,6 +58,15 @@ testY = 10
 # Game Over
 over_font = pygame.font.Font('freesansbold.ttf', 64)
 
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
