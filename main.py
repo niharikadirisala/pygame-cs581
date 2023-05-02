9ab983304079e825ff241946f6a01e919a1febcf
Niharika <dirisalaniharika1999@gmail.com>
2023-05-02 08:50:21 +0530

    Added key events

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change=0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 # Game Loop
 running = True
@@ -27,8 +28,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            playerX_change = -5
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 5
 
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerX_change = 0
 
+            
     screen.fill((0,0,0))
     pygame.display.update()
 
